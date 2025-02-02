78117535c82addf8a20e6f9576910a86a1c0f547
tech <ghargeakash07@gmail.com>
2025-02-02 11:38:34 +0530

    Fixed Changes Bug hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,8 @@ public class Hello
    public static void main(String x[])
    {
       System.out.println("Shree Swami Samartha___");
+	  System.out.println("Shree Sidhnath Prasanna___");
+	  System.out.println("Shree Swami Samartha___");
+	  
    }
 }
